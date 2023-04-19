2f22cff3731314937d60b8899a7c2935265e68ab
Florian SAVALLE <florian.savalle@viacesi.fr>
2023-04-19 10:23:21 +0200

    config angular

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainFrameComponent } from './main-frame/main-frame.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'mainframe', component: MainFrameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
