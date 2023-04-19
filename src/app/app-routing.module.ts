a764ad248cf9d842fd68c9ed46e86157527faa43
Florian SAVALLE <florian.savalle@viacesi.fr>
2023-04-19 13:31:07 +0200

    mainframe init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainframeComponent } from './mainframe/mainframe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "test", component: MainframeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+})  
 export class AppRoutingModule { }
