a9e72b18cdb8c0df10b8313f7a4f5921cd47032e
Florian SAVALLE <florian.savalle@viacesi.fr>
2023-05-22 11:06:54 +0200

    Misc translations done

--- src/app/miscinfos/miscinfos.component.ts
+++ src/app/miscinfos/miscinfos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslationService } from '../translation.service';
 
 @Component({
@@ -6,7 +6,15 @@ import { TranslationService } from '../translation.service';
   templateUrl: './miscinfos.component.html',
   styleUrls: ['./miscinfos.component.css']
 })
-export class MiscinfosComponent {
-  constructor(public translationService: TranslationService) {}
+export class MiscinfosComponent implements OnInit {
+  dataLoaded = false;
+
+  constructor(public translationService: TranslationService) { }
+
+  ngOnInit() {
+    this.translationService.dataLoaded$.subscribe(isLoaded => {
+      this.dataLoaded = isLoaded;
+    });
+  }
 
 }
